89b76e1624bfb4914f295f42443525a3b8b221db
fouR <irgiamysebi@gmail.com>
2021-02-06 00:32:46 +0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
-local isSprinting = false
+local isSprinting, isSwimming, isUnderwater = false, false, false
 
 Citizen.CreateThread(function()
 	while true do
@@ -8,11 +8,14 @@ Citizen.CreateThread(function()
 		if isSprinting then
 			letSleep = false
 			if stamina == 100 then
-			RequestAnimDict("re@construction")
-			while not HasAnimDictLoaded("re@construction") do
-			Citizen.Wait(100)
-			end
-			TaskPlayAnim(PlayerPedId(), "re@construction", "out_of_breath", 8.0, 8.0, -1, 32, 0, false, false, false)
+			local playerPed = PlayerPedId()
+				if not isSwimming and not isUnderwater then
+					RequestAnimDict("re@construction")
+					while not HasAnimDictLoaded("re@construction") do
+					Citizen.Wait(100)
+					end			
+					TaskPlayAnim(PlayerPedId(), "re@construction", "out_of_breath", 8.0, 8.0,-1, 32, 0, false, false, false)
+				end
 			end
 		end
 		if letSleep then
@@ -25,6 +28,8 @@ Citizen.CreateThread(function()
     while true do
 	local lPed = GetPlayerPed(-1)
 	isSprinting = IsPedSprinting(lPed)
+	isSwimming = IsPedSwimming(lPed)
+	isUnderwater = IsPedSwimmingUnderWater(lPed)
         Citizen.Wait(500)
     end
 end)
